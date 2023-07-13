27f3342d621752d43c711cae1dc61bf13acc531f
Subhabrata Banerjee <63787106+subhabratabanerjee716@users.noreply.github.com>
2023-07-13 11:37:24 +0530

    Create App.java

---
+++ App.java
@@ -0,0 +1,74 @@
+package Q18.HibernateQuestion1;
+
+import java.util.List;
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    
+	public static void main( String[] args )
+    {
+        
+    	Configuration cfg = new Configuration().configure("hibernate.cfg.xml");
+    	
+    	SessionFactory factory = cfg.buildSessionFactory();
+    	
+    	
+    	Student s1 = new Student();
+    	
+    	Student s2 = new Student();
+    	
+    	Student s3 = new Student();
+    	
+    	s1.setName("kalpeshari Banerjee");
+    	s1.setCourse("VLSI");
+    	
+    	
+    	s2.setName("Anand Mishra");
+    	s2.setCourse("SpringBoot Developer Course");
+    	
+    	
+    	s3.setName("Arnav Anand");
+    	s3.setCourse("SpringBoot Developer Course");
+    	Session s = factory.openSession();
+    	
+    	Transaction tx = s.beginTransaction();
+    	
+    	s.save(s3);
+    	s.save(s2);
+    	s.save(s3);
+    	
+    	tx.commit();
+    	
+    	Query<Student> q = s.createQuery("from Student");
+    	
+    	List<Student> l=q.list();
+    	
+    	
+  
+    	for(Student e:l) {
+    		
+    		System.out.println(e.getId() + " " + e.getName() +" " + e.getCourse());
+    		
+    	}
+    	
+    	
+    	System.out.println("Successfully Saved");
+    	
+    	
+    	factory.close();
+    	s.close();
+    }
+
+	
+}
